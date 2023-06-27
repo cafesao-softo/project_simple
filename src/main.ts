19ba914330fac644aebf17939f2397a436b028ce
cafesao <my@cafesao.net>
2023-06-27 18:40:13 -0300

    feat: @nest/typeorm

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./modules/app.module"
-import { AppDataSource } from "./services/connect"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  try {
-    console.log("DB starting...")
-    await AppDataSource.initialize()
-    console.log("DB initialized")
-    await app.listen(process.env.PORT || 3000)
-  } catch (error) {
-    throw error
-  }
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -5,10 +5,30 @@ import { CityController } from "../controllers/city.controller"
 import { DistrictController } from "../controllers/district.controller"
 import { AppService } from "../services/app.service"
 import { ConfigModule } from "@nestjs/config"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { DistrictModel } from "src/models/District"
+import { CityModel } from "src/models/City"
+import { StateModel } from "src/models/State"
+import { CreateController } from "src/controllers/create.controller"
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.Node_ENV === "dev" ? "localhost" : "db",
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [DistrictModel, CityModel, StateModel],
+      synchronize: true,
+      logging: false,
+      autoLoadEntities: true
+    })
+  ],
   controllers: [
+    CreateController,
     HeathcheckController,
     StateController,
     CityController,
