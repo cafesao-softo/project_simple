4ebce378f3b65c0f04e9378f6e2eba5eaa779f43
cafesao <my@cafesao.net>
2023-06-27 17:18:54 -0300

    feat: add connect db in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./modules/app.module"
+import { AppDataSource } from "./services/connect"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT || 3000)
+  try {
+    console.log("DB starting...")
+    await AppDataSource.initialize()
+    console.log("DB initialized")
+    await app.listen(process.env.PORT || 3000)
+  } catch (error) {
+    throw error
+  }
 }
 bootstrap()
