5295375859c090ee8d9beb3e2821782fea565f9c
cafesao <my@cafesao.net>
2023-06-29 11:20:27 -0300

    feat: add environment

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,11 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PORT: string
+      Node_ENV: string
+      MYSQL_PORT: string
+      MYSQL_USER: string
+      MYSQL_PASSWORD: string
+      MYSQL_DATABASE: string
     }
   }
 }
